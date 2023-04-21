e2e58e215f5ccc0acdac044f74efab5961af5909
Vladut Mihai Poncea <vladponcea@icloud.com>
2023-04-21 10:09:33 +0300

    added apiKey to url

--- Sources/APIService/APIRequest.swift
+++ Sources/APIService/APIRequest.swift
@@ -25,14 +25,13 @@ public final class APIRequest {
             }
         }
         
+        string += "?apiKey=\(constants.apiKey)"
+        
         if !queryParameters.isEmpty {
-            string += "?"
-            
             let argumentString = queryParameters.compactMap({
                 guard let value = $0.value else { return nil }
                 return "\($0.name)=\(value)"
             }).joined(separator: "&")
-            
             string += argumentString
         }
         
